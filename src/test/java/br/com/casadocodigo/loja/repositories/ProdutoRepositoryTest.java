ebbab1803ade03e4961f8487c364ea81412dd21a
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2017-08-12 16:23:38 -0300

    Criando a classe ProdutoRepositoryTest

--- src/test/java/br/com/casadocodigo/loja/repositories/ProdutoRepositoryTest.java
+++ src/test/java/br/com/casadocodigo/loja/repositories/ProdutoRepositoryTest.java
@@ -1,4 +1,4 @@
-package casadocodigo;
+package br.com.casadocodigo.loja.repositories;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,9 +8,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.casadocodigo.loja.builders.ProdutoBuilder;
 import br.com.casadocodigo.loja.conf.JPATestConfiguration;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.repositories.ProdutoRepository;
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = JPATestConfiguration.class, loader = AnnotationConfigContextLoader.class)
@@ -18,16 +20,15 @@ import br.com.casadocodigo.loja.repositories.ProdutoRepository;
 public class ProdutoRepositoryTest {
 
 	@Autowired
-	private ProdutoRepository repo;
+	private ProdutoRepository produtoRepository;
 
 	@Test
 	public void testSaveProduto() {
-		Produto p = new Produto();
-		p.setTitulo("teste");
-		p.setDescricao("teste");
-		p.setPaginas(333);
-		Produto save = repo.save(p);
-		System.out.println(save.getId());
+		Produto produtoSaved = produtoRepository.save(new ProdutoBuilder("Design Patters com Java", 
+																		 "Livro sobre Design Patters com Java",
+																		  444).build());
+		
+		assertNotNull("O valor do id não deve ser nulo!",produtoSaved.getId());
 	}
 
 }
